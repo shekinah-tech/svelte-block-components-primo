28ae1b36c9e5d972755a689a17435170a0cd7286
Ezra Lalonde <ezra@usefuliftrue.com>
2023-07-21 15:54:52 -0600

    remove tester dep, try second time with tailwind

--- src/lib/Test.svelte
+++ src/lib/Test.svelte
@@ -2,15 +2,8 @@
   <script src="https://cdn.tailwindcss.com"></script>
 </svelte:head>
 
-<script>
-  import {Tester} from "https://raw.githubusercontent.com/mateomorris/primo-components/de0a89dae443d902b963374ab9ef281acbe4bf15/dist/Heading.svelte"
-</script>
-
 <div>
-  <Tester>
-    <slot><!-- optional fallback --></slot>
-  </Tester>
-  <h1 class="text-3xl">
+  <h1 class="text-3xl underline">
     <slot><!-- optional fallback --></slot>
   </h1>
 </div>

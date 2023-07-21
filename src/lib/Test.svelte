28ed557f9f8600f8c0f3fbb36f2697902bbcfc3d
Ezra Lalonde <ezra@usefuliftrue.com>
2023-07-21 15:44:43 -0600

    add remote tailwind?

--- src/lib/Test.svelte
+++ src/lib/Test.svelte
@@ -1,17 +1,24 @@
+<svelte:head>
+  <script src="https://cdn.tailwindcss.com"></script>
+</svelte:head>
 
 <script>
   import {Tester} from "https://raw.githubusercontent.com/mateomorris/primo-components/de0a89dae443d902b963374ab9ef281acbe4bf15/dist/Heading.svelte"
-  import "../app.css"
 </script>
 
-<Tester>
-<slot><!-- optional fallback --></slot>
-</Tester>
+<div>
+  <Tester>
+    <slot><!-- optional fallback --></slot>
+  </Tester>
+  <h1 class="text-3xl">
+    {hello}
+  </h1>
+</div>
 
 
 <style>
-	h1 {
-		font-size: 4rem;
-		font-weight: 500;
-	}
+h1 {
+  font-size: 4rem;
+  font-weight: 500;
+}
 </style>
